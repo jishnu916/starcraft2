d17e7c1a6f0db0fa2a7a23d5774a41816d8ca1d5
jishnu916 <34061172+jishnu916@users.noreply.github.com>
2019-01-03 11:47:07 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+#include<string>
+
+int main(int argc, char *argv[]) {
+
+    string race = arg[1];
+    string buildlist = arg[2];
+
+
+
+
+
+
+
+}
---
+++ resource.cpp
@@ -0,0 +1,18 @@
+class Resources {
+
+
+public :
+        double minerals;
+        double vespene;
+        int supply;
+        int energy;
+
+        Resources(int minerals,int vespene,int supply,int energy){
+
+                this->minerals=minerals;
+                this->vespene=vespene;
+                this->supply=supply;
+                this->energy=energy;
+        }
+
+};
